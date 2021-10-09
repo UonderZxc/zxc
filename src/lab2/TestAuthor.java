dccf88ab0d83ec7888cfce0e3f4283a92a36c877
UonderZxc <91430470+UonderZxc@users.noreply.github.com>
2021-10-09 10:16:27 +0300

    Add files via upload

---
+++ src/lab2/TestAuthor.java
@@ -0,0 +1,8 @@
+package lab2;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author n = new Author ("Petya","Petya@email.ru",'m');
+        System.out.println(n);
+    }
+}
---
+++ src/lab3_2/Movable.java
@@ -0,0 +1,9 @@
+package lab3_2;
+
+public interface Movable {
+    void moveUp();
+    void moveDown();
+    void moveLeft();
+    void moveRight();
+}
+
